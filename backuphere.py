f30294119e13275b4189402a2a651b279b34a4f1
Thomas Lauritzen <tommer.rl@live.dk>
2017-06-09 03:58:20 +0200

    Script as it is now

---
+++ backuphere.py
@@ -0,0 +1,15 @@
+#Backup files chosen folder. Making a backup folder in that folder.
+import os, sys
+from distutils.dir_util import copy_tree
+
+#Select folder and check if it exists
+path = sys.argv[1]
+if os.path.exists(path):
+    print("\""+path+"\""+" exists.")
+
+#Check if _Backup folder exists if not, copy_tree into a new _Backup folder which will be created.
+if os.path.exists(path+"\\_Backup"):
+    print("\""+path+"\\_Backup\""+" already exists, please check the current \"_Backup\" folder out")
+else:
+    copy_tree(path, path+"\\_Backup")
+    print("Your files have now been backed up! :)")
